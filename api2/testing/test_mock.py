6948dc9d2642e252de9a7bc8ecdbcd96ce847c97
Ryan Wright <68053939+RWright1992@users.noreply.github.com>
2022-05-09 10:46:52 +0100

    Update test_mock.py

--- api2/testing/test_mock.py
+++ api2/testing/test_mock.py
@@ -8,3 +8,10 @@ class TestBase(TestCase):
     def create_app(self):
         return app
 
+class TestRandom(TestBase):
+    def test_dog(self):
+        with patch ('random.randint') as r:
+            r.return_value = 0
+
+            response = self.client.get(url_for('get_animal'))
+            self.assertIn(b'Dog', response.data)
